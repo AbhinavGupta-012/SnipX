422c17a21fbf2663464a9069dd9e40b66fe14602
AbhinavVerma07 <abhinavverma269@gmail.com>
2024-09-06 10:03:09 +0530

    made changes to login page

--- src/js/login.js
+++ src/js/login.js
@@ -9,3 +9,28 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+document.getElementById('signUpBtn').addEventListener('click', function(event) {
+  event.preventDefault();
+
+  const username = document.getElementById('username').value.trim();
+  const email = document.getElementById('email').value.trim();
+  const password = document.getElementById('password').value.trim();
+
+  if (username === "" || email === "" || password === "") {
+      alert("Please fill in all the fields.");
+  } else {
+       alert("Form submitted successfully!");
+  }
+});
+document.getElementById('loginBtn').addEventListener('click', function(event) {
+  event.preventDefault(); 
+
+  const loginUsername = document.getElementById('loginUsername').value.trim();
+  const loginPassword = document.getElementById('loginPassword').value.trim();
+
+  if (loginUsername === "" || loginPassword === "") {
+      alert("Please fill in both username and password.");
+  } else {
+      alert("Login successful!");
+  }
+});
