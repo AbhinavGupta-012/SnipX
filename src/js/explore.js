f40877861df2c4afd4872bb44c52f056bc8e67e1
AbhinavGupta-012 <abhinavgupta1702@gmail.com>
2024-09-09 13:52:51 +0530

    .

--- src/js/explore.js
+++ src/js/explore.js
@@ -4,11 +4,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
     tabItems.forEach(tab => {
         tab.addEventListener('click', function () {
-            // Remove active class from all tabs and contents
             tabItems.forEach(item => item.classList.remove('active'));
             tabContents.forEach(content => content.classList.remove('active'));
 
-            // Add active class to the clicked tab and corresponding content
             tab.classList.add('active');
             document.getElementById(tab.getAttribute('data-tab')).classList.add('active');
         });
